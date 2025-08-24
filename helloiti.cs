06a4879c4244d4fac8b47cbd32ff7c3144256a3c
ahmed-salama07 <ahmed_salama99@outlook.com>
2025-08-24 16:04:06 +0300

    Added printHelloGit to Hello ITI class

--- helloiti.cs
+++ helloiti.cs
@@ -3,6 +3,17 @@ class HelloITI
 {
     public static void Main()
     {
+        PrintHelloITI();
+        printHelloGit();
+    }
+
+    public void PrintHelloITI()
+    {
         Console.WriteLine("Hello, ITI");
+    }
+
+    public void printHelloGit()
+    {
+        Console.WriteLine("Hello, Git");
     }
 }
